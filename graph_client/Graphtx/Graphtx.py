eabfac78c15a49ecd00463734bd5ae3ed267e41e
Jiulong Lan <jiulong.lan@shopee.com>
2020-12-03 12:13:22 +0800

    second commit

--- graph_client/Graphtx/Graphtx.py
+++ graph_client/Graphtx/Graphtx.py
@@ -1,6 +1,13 @@
 class Graphtx:
     def __init__(
         self,
-        url
+        url,
+        name,
     ):
+    """constructor
+
+    :param url: url
+    :param name: name
+    """
     self.__url = url
+    self.__name = name
